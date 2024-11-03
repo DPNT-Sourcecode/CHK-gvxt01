17df11623eb366dd22a4d24ff33d7b29a86c2565
sbx_user1051 <sbx_user1051@169.254.38.249>
2024-11-03 18:12:43 +0000

    Sun Nov 03 18:12:43 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,4 +4,4 @@
 # friend_name = unicode string
 def hello(friend_name):
 
-    return 'hello world'
+    return 'Hello, World'
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,4 +3,4 @@ from solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello(self):
-        assert hello_solution.hello('test-string')== 'hello world'
+        assert hello_solution.hello('test-string')== 'Hello, World'
