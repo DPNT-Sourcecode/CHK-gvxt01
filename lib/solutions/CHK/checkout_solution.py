ed4adfc1d3a01e636977f4369f315441670d7f87
sbx_user1051 <sbx_user1051@169.254.38.249>
2024-11-03 19:02:54 +0000

    Sun Nov 03 19:02:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,39 +1,3 @@
-"""
-CHK_R1
-ROUND 1 - Our supermarket
-The purpose of this challenge is to implement a supermarket checkout that calculates the total price of a number of items.
-
-In a normal supermarket, things are identified using Stock Keeping Units, or SKUs. 
-In our store, we'll use individual letters of the alphabet (A, B, C, and so on). 
-Our goods are priced individually. 
-In addition, some items are multi-priced: buy n of them, and they'll cost you y pounds. 
-
-
-For example, item A might cost 50 pounds individually, but this week we have a special offer: 
- buy three As and they'll cost you 130.
-
-Our price table and offers: 
-+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-| B    | 30    | 2B for 45      |
-| C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+
-
-
-Notes: 
- - For any illegal input return -1
-
-In order to complete the round you need to implement the following method:
-     checkout(String) -> Integer
-
-Where:
- - param[0] = a String containing the SKUs of all the products in the basket
- - @return = an Integer representing the total checkout value of the items 
-
-"""
 from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -45,20 +9,56 @@ from collections import Counter
 
     before calculating check all the input values are in the price list
 """
+
+
+"""
+CHK_R2
+ROUND 2 - More offers
+The checkout feature is great and our supermarket is doing fine. Is time to think about growth.
+Our marketing teams wants to experiment with new offer types and we should do our best to support them.
+
+We are going to sell a new item E.
+Normally E costs 40, but if you buy 2 of Es you will get B free. 
+How cool is that ? Multi-priced items also seemed to work well so we should have more of these.
+
+Our price table and offers: 
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
+
+
+Notes: 
+ - The policy of the supermarket is to always favor the customer when applying special offers.
+ - All the offers are well balanced so that they can be safely combined.
+ - For any illegal input return -1
+
+In order to complete the round you need to implement the following method:
+     checkout(String) -> Integer
+
+Where:
+ - param[0] = a String containing the SKUs of all the products in the basket
+ - @return = an Integer representing the total checkout value of the items 
+"""
+
+
+
 def checkout(skus):
     if not isinstance(skus, str):
         return -1
     
-    # Not known whether the input is a list of capital letters, comma separated or just one word so separation will be needed to handle both
-
     priceDict = {
         'A' : 50,
         'B' : 30,
         'C' : 20,
-        'D' : 15
+        'D' : 15,
+        'E' : 40
     }
-
-    # Special buys. dict val is a tuple with the number and the value
 
     specialDict = {
         'A' : (3, 130),
@@ -76,6 +76,8 @@ def checkout(skus):
     
     resultsFreqs = Counter(units)
     total = 0
+
+    # So no there are combo offers 
     for item in resultsFreqs.keys():
         specialExists =  specialDict.get(item, None) != None
         if not specialExists:
@@ -94,6 +96,7 @@ def checkout(skus):
 
 test = checkout('AbC')
 print(test)
+
 
 
 
