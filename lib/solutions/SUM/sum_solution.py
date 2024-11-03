8354a8610be397faf8ff6efa9981706c42c3800c
sbx_user1051 <sbx_user1051@169.254.38.249>
2024-11-03 18:05:40 +0000

    Sun Nov 03 18:05:40 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     assert isinstance(x, int) and isinstance(y, int), "inputs must be of type integer"
-    assert x > 0 and 
+    assert x >= 0 and x <= 100, "x must be between 0 and 100"
+    assert y >= 0 and y <= 100, "y must be between 0 and 100"
+
+    return x + y
+
 
